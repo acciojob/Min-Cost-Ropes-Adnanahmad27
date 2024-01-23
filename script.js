552affe05456842191de6206e3baca8907f2b239
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-23 16:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function mincost(arr)
 { 
-	// sort
-	// take out 2 ele and make it one , push back to array
 	let sum = 0;
 	while(arr.length > 1){
 		
 		arr.sort( (a,b) => {
-		return b - a;
+		return a - b;
 		});
 		
-		let a = arr.pull();
-		let b = arr.pull();
+		let a = arr.shift();
+		let b = arr.shift();
 
 		sum += a+b;
 		arr.push(a+b);
